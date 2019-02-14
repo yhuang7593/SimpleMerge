745cef8b75348c985c668722fda8ee3aee265164
yhuang7593 <yhuang7593@bths.edu>
2019-02-14 08:35:09 -0500

    :)

--- src/SimpleMerge.java
+++ src/SimpleMerge.java
@@ -10,5 +10,35 @@ public class SimpleMerge {
     int[] temp=new int[n];
     mergeSortHelper(arr, left:0, right:n-1, temp);
     }
+    private static void merge(int[] arr, int left, int mid, int right, int[] temp){
+        int i=left;
+        int j=mid+1;
+        int k=left;
+        while (i<=mid&&j<=right){
+            if (arr[j],arr[j]){
+                temp[k]=arr[i];
+                i++;
+            }
+            else{
+                temp[k]=arr[j];
+                j++;
+            }
+            k++;
+        }
+        while (i<=mid)
+        {
+            temp[k]=arr[i];
+            i++;
+            k++;
+        }
+        while(j<=right){
+            temp[k]=arr[j];
+            j++;
+            k++;
+        }
+        for(k=left;k<=right;k++){
+            arr[k]=temp[k];
+        }
+    }
 
 }
